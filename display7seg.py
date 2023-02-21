f78a3b83eb2df16de94a1f22a8ccd0995bd91f9f
eulises <eulisesbrazon@gmail.com>
2023-02-21 17:32:41 -0400

    corrections

--- display7seg.py
+++ display7seg.py
@@ -60,240 +60,73 @@ class Display:
                 self.display[i].on()
             bit = bit << 1
         
-"""
-Metodo agregado
-"""
+
+#Codigo agregado by: Eulises Brazonm
+
+                   #char,'C', 'A',
+        self.dictionary = [['0', int('3f',16),int('40',16)],
+                    ['1', int('06',16),int('79',16)],
+                    ['2', int('5b',16),int('24',16)],
+                    ['3', int('4f',16),int('30',16)],
+                    ['4', int('66',16),int('19',16)],
+                    ['5', int('6D',16),int('12',16)],
+                    ['6', int('7D',16),int('02',16)],
+                    ['7', int('07',16),int('78',16)],
+                    ['8', int('7f',16),int('00',16)],
+                    ['9', int('67',16),int('18',16)],
+                    ['A', int('77',16),int('08',16)],
+                    ['b', int('7c',16),int('03',16)],
+                    ['C', int('39',16),int('46',16)],
+                    ['c', int('58',16),int('27',16)],
+                    ['d', int('5e',16),int('21',16)],
+                    ['E', int('79',16),int('06',16)],
+                    ['F', int('71',16),int('0e',16)],
+                    ['f', int('71',16),int('0e',16)],
+                    ['G', int('3d',16),int('42',16)],
+                    ['H', int('76',16),int('09',16)],
+                    ['h', int('74',16),int('0b',16)],
+                    ['i', int('30',16),int('4f',16)],
+                    ['J', int('1e',16),int('61',16)],
+                    ['j', int('1e',16),int('61',16)],
+                    ['L', int('38',16),int('47',16)],
+                    ['l', int('38',16),int('47',16)],
+                    ['P', int('73',16),int('0c',16)],
+                    ['p', int('73',16),int('0c',16)],
+                    ['q', int('67',16),int('18',16)],
+                    ['r', int('31',16),int('4e',16)],
+                    ['S', int('6f',16),int('12',16)],
+                    ['U', int('3e',16),int('41',16)],
+                    ['u', int('1c',16),int('63',16)],
+                    ['Y', int('6e',16),int('11',16)]]
         
-    def show_char(self, digit, location):
+    def show_char(self, char, location):
         
-        #evaluo el valor qeu estoy recibiedo
-        if digit == '0':
-            #una vez que reconosco el caracter,asigno su valor correspondiente
-            if self.kind.upper() == 'C':
-                numbers = (int('3f',16))
-            elif self.kind.upper() == 'A':
-                numbers = (int('40',16))
-            else:
-                return
-        elif digit == '1':
-            if self.kind.upper() == 'C':
-                numbers = (int('06',16))
-            elif self.kind.upper() == 'A':
-                numbers = (int('79',16))
-            else:
-                return
-        elif digit =='2':
-            if self.kind.upper() == 'C':
-                numbers = (int('5b',16))
-            elif self.kind.upper() == 'A':
-                numbers = (int('24',16))
-            else:
-                return
-        elif digit == '3':
-            if self.kind.upper() == 'C':
-                numbers = (int('4f',16))
-            elif self.kind.upper() == 'A':
-                numbers = (int('30',16))
-            else:
-                return
-        elif digit == '4':
-            if self.kind.upper() == 'C':
-                numbers = (int('66',16))
-            elif self.kind.upper() == 'A':
-                numbers = (int('19',16))
-            else:
-                return
-        elif digit == '5':
-            if self.kind.upper() == 'C':
-                numbers = (int('6D',16))
-            elif self.kind.upper() == 'A':
-                numbers = (int('12',16))
-            else:
-                return
-        elif digit == '6':
-            if self.kind.upper() == 'C':
-                numbers = (int('7D',16))
-            elif self.kind.upper() == 'A':
-                numbers = (int('02',16))
-            else:
-                return
-        elif digit == '7':
-            if self.kind.upper() == 'C':
-                numbers = (int('07',16))
-            elif self.kind.upper() == 'A':
-                numbers = (int('78',16))
-            else:
-                return
-        elif digit == '8':
-            if self.kind.upper() == 'C':
-                numbers = (int('7f',16))
-            elif self.kind.upper() == 'A':
-                numbers = (int('00',16))
-            else:
-                return
-        elif digit == '9':
-            if self.kind.upper() == 'C':
-                numbers = (int('67',16))
-            elif self.kind.upper() == 'A':
-                numbers = (int('18',16))
-            else:
-                return
-        elif digit =='A':
-            if self.kind.upper() == 'C':
-                numbers = (int('77',16))
-            elif self.kind.upper() == 'A':
-                numbers = (int('08',16))
-            else:
-                return
-        elif digit == 'b':
-            if self.kind.upper() == 'C':
-                numbers = (int('7c',16))
-            elif self.kind.upper() == 'A':
-                numbers = (int('03',16))
-            else:
-                return
-        elif digit == 'C':
-            if self.kind.upper() == 'C':
-                numbers = (int('39',16))
-            elif self.kind.upper() == 'A':
-                numbers = (int('46',16))
-            else:
-                return
-        elif digit =='c':
-            if self.kind.upper() == 'C':
-                numbers = (int('58',16))
-            elif self.kind.upper() == 'A':
-                numbers = (int('27',16))
-            else:
-                return
-        elif digit == 'd':
-            if self.kind.upper() == 'C':
-                numbers = (int('5e',16))
-            elif self.kind.upper() == 'A':
-                numbers = (int('21',16))
-            else:
-                return
-        elif digit == 'E':
-            if self.kind.upper() == 'C':
-                numbers = (int('79',16))
-            elif self.kind.upper() == 'A':
-                numbers = (int('06',16))
-            else:
-                return
-        elif digit == 'F' or 'f':
-            if self.kind.upper() == 'C':
-                numbers = (int('71',16))
-            elif self.kind.upper() == 'A':
-                numbers = (int('0e',16))
-            else:
-                return
-        elif digit == 'G':
-            if self.kind.upper() == 'C':
-                numbers = (int('3d',16))
-            elif self.kind.upper() == 'A':
-                numbers = (int('42',16))
-            else:
-                return
-        elif digit == 'H':
-            if self.kind.upper() == 'C':
-                numbers = (int('76',16))
-            elif self.kind.upper() == 'A':
-                numbers = (int('09',16))
-            else:
-                Return
-        elif digit == 'h':
-            if self.kind.upper() == 'C':
-                numbers = (int('74',16))
-            elif self.kind.upper() == 'A':
-                numbers = (int('0b',16))
-            else:
-                    eturn
-        elif digit == 'i':
-            if self.kind.upper() == 'C':
-                numbers = (int('30',16))
-            elif self.kind.upper() == 'A':
-                numbers = (int('4f',16))
-            else:
-                return
-        elif digit == 'J' or 'j':
-            if self.kind.upper() == 'C':
-                numbers = (int('1e',16))
-            elif self.kind.upper() == 'A':
-                numbers = (int('61',16))
-            else:
-                return
-        elif digit == 'L'or 'l':
-            if self.kind.upper() == 'C':
-                numbers = (int('38',16))
-            elif self.kind.upper() == 'A':
-                numbers = (int('47',16))
-            else:
-                return
-        elif digit == 'P'or 'p':
-            if self.kind.upper() == 'C':
-                numbers = (int('73',16))
-            elif self.kind.upper() == 'A':
-                numbers = (int('0c',16))
-            else:
-                return
-        elif digit == 'q':
-            if self.kind.upper() == 'C':
-                numbers = (int('67',16))
-            elif self.kind.upper() == 'A':
-                numbers = (int('18',16))
-            else:
-                return
-        elif digit == 'r':
-            if self.kind.upper() == 'C':
-                numbers = (int('31',16))
-            elif self.kind.upper() == 'A':
-                numbers = (int('4e',16))
-            else:
-                return
-        elif digit == 'S':
-            if self.kind.upper() == 'C':
-                numbers = (int('6f',16))
-            elif self.kind.upper() == 'A':
-                numbers = (int('12',16))
-            else:
-                return
-        elif digit == 'U':
-            if self.kind.upper() == 'C':
-                numbers = (int('3e',16))
-            elif self.kind.upper() == 'A':
-                numbers = (int('41',16))
-            else:
-                return
-        elif digit == 'u':
-            if self.kind.upper() == 'C':
-                numbers = (int('1c',16))
-            elif self.kind.upper() == 'A':
-                numbers = (int('63',16))
-            else:
+        #evaluo el valor que estoy recibiedo
+  
+        for word in self.dictionary:
+            if char == word[0]:
+                if self.kind.upper() == 'C':
+                    symbol= word[1]
+                if self.kind.upper() == 'A':
+                    symbol= word[2]
+                else:
                     return
-        elif digit == 'Y':
-            if self.kind.upper() == 'C':
-                numbers = (int('6e',16))
-            elif self.kind.upper() == 'A':
-                numbers = (int('11',16))
-            else:
-                return    
-        else:
-            #si no corresponde con ninguno termino la ejecucion
-            return
-        
-        
-        #realizo el recorrido bit a bit
-        bit = 1;
-        
-        for i in range(7):
-            if (numbers  & bit) == 0: #evaluo si en la posicion del bit se encuentra un 0
-                self.display[i].off()
-            else:
-                self.display[i].on()
-            bit = bit << 1 #desplazo el bit de comparazion hacia la izquierda
+        #en caso de no encontrarse el valor arrojara un error
+        try:
+            bit = 1;
+            for i in range(7):
+                if (symbol  & bit) == 0: #evaluo si en la posicion del bit se encuentra un 0
+                    self.display[i].off()
+                else:
+                    self.display[i].on()
+                bit = bit << 1 #desplazo el bit de comparazion hacia la izquierda
             
-        self.transistors[location].on()
-        utime.sleep_ms(1)
-        self.transistors[location].off()
+            #enciendo el digito durante un breve instante de tiempo
+            self.transistors[location].on()
+            utime.sleep_ms(1)
+            self.transistors[location].off()
+            
+        except Exception as e:
+            print("Error:", e)
+        
 
